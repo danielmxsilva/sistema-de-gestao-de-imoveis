42d1b0b0db9189258c8f66153a03cde4160e41ba
Daniel Mateus X Silva <danielmxsilva@gmail.com>
2021-10-18 11:30:35 -0300

    FINALIZADA AULA 5/8

--- painel/pages/editar-empreendimento.php
+++ painel/pages/editar-empreendimento.php
@@ -30,7 +30,23 @@
 
 <div class="box-content" style="margin-top:40px;">
 	<img src="<?php echo INCLUDE_PATH_PAINEL;?>img/editar-depoimento-gray.png">
-	<h2>Editar Empreendimento</h2>
+
+	<h2>Empreendimento: </h2>
+
+	<div class="wraper10">
+
+		<div class="row3">
+			<h2 class="title-blue">Imagem do empreendimento</h2>
+			<img src="<?php echo INCLUDE_PATH_PAINEL?>uploads/<?php echo $categoria['imagem']?>">
+		</div><!--row3-->
+
+		<div class="row7">
+			<h2 class="title-blue">Informações do empreendimento</h2>
+			<p>Empreendimento: <?php echo $categoria['nome']?></p>
+			<p>Tipo: <?php echo $categoria['tipo']?></p>
+		</div><!--row7-->
+
+	</div><!--wraper10-->
 
 
 </div><!--box-content-->
